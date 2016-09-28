c9bcab8bafc1b90fc7d2839c0d0268517f62431e
jiang.gaungjin@niuhome.com <vip1225335417>
2016-09-28 18:40:31 +0800

    -

---
+++ IOSDemo/IOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDemo
+//
+//  Created by niuhome_vip on 16/9/28.
+//  Copyright © 2016年 niuhome_vip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
